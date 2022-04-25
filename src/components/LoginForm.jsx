317965f3a80692e584dad26525ff3ac51ffe9b3b
David Montaño <cuatro04sr@gmail.com>
2022-04-25 14:45:57 -0500

    feat: add front login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,21 +3,28 @@ import styles from '../styles/login-form.module.css'
 
 export default function LoginForm() {
     return (
-        <Formik
-            initialValues={{
-                username: '',
-                password: '',
-                type: '',
-            }}
-            onSubmit={() => {
+        <div className={styles.login_box + ' p-3'}>
+            <h1 className="display-6 mb-3">Login</h1>
 
-            }}
-        >
-            <Form>
-                <Field id="username" name="username" placeholder="Username" />
-                <Field type="password" id="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </Form>
-        </Formik>
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: '',
+                }}
+                onSubmit={() => {
+
+                }}
+            >
+                <Form>
+                    <div className="mb-3">
+                        <Field className="form-control" id="username" name="username" placeholder="Username" aria-describedby="usernameHelp"/>
+                    </div>
+                    <div className="mb-3">
+                        <Field className="form-control" id="password" name="password" placeholder="Password" type="password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </Form>
+            </Formik>
+        </div>
     )
 }
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -6,9 +6,10 @@ const Home = () => {
         <div>
             <Head>
                 <title>Login Page</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main>
+            <main className="vh-100 d-flex justify-content-center align-items-center">
                 <LoginForm/>
             </main>
         </div>
