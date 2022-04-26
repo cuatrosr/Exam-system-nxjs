7e5bcd6bfe8bac2a5388f65c47d8e764a3043900
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-04-26 10:34:48 -0500

    Trying

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,10 @@ import styles from '../styles/navForm.css'
 
 export default function NavForm(){
     return(
-        <body>
-            <header>
-                <nav>
-                    <ul className={styles.header}>
-                        <li><a href="#">Options</a> </li>
-                    </ul>
-                </nav>
-            </header>
-        </body>
+        <div className={styles.navbar}>
+            <div>
+                <a href="#">HOME</a>
+            </div>
+        </div>
     );
 }
