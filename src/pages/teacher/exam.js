77ff8724757b57ec2be4eb8b5a1d40d3a0cca869
Daniela Olarte <danielaolarte123@gmail.com>
2022-04-26 09:10:07 -0500

    form fished

--- src/pages/teacher/exam.js
+++ src/pages/teacher/exam.js
@@ -9,15 +9,15 @@ export default function createExam(){
             <Header title='Create exam'/>
             <main className={styles.main}>
                 <form>
-                    <div class='mb-3'>
+                    <div class='text-center'>
                         <label for='formularyName' class='form-label'>Nombre del formulario</label>
                         <input type='text' class="form-control" id='formularyName'/>
                     </div>
-                    <div class='mb-3'>
+                    <div class='text-center'>
                         <label for='accessCode' class='form-label'>Código de acceso</label>
                         <input type='password' class='form-control' id='accessCode'/>
                     </div>
-                    <div class='mb-3'>
+                    <div class='text-center'>
                         <label for='description' class='form-label'>Descripción</label>
                         <input type='desc' class='form-control' id='descrip'/>
                     </div>
@@ -30,8 +30,14 @@ export default function createExam(){
                                 <input type='textquest' class="form-control" id='question1'/>
                             </div>
                             <div class='mb-3'>
-                                <label for='Respuesta1' class='form-label'>Respuestas:</label>
-                                <input type='textresp1' class="form-control" id='response1'/>
+                                <label for='Respuestas' class='form-label'>Respuestas:</label>
+                                <div class="d-flex">a)<input type='textresp1' class="form-control" id='response1'/></div>
+                                <div class="d-flex">b)<input type='textresp2' class="form-control" id='response2'/></div>
+                                <div class="d-flex">c)<input type='textresp3' class="form-control" id='response3'/></div>
+                                <div class="d-flex">d)<input type='textresp4' class="form-control" id='response4'/></div>
+                                <button type='btn' class='text-left'>
+                                    Añadir pregunta
+                                </button>
                             </div>
                             <div class='text-center'>
                                 <button type='btn' class='btn btn-primary'>
