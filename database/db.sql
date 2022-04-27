89a4c8c0ac47c16e4919d53301c20ac8c5eabfe4
David Montaño <cuatro04sr@gmail.com>
2022-04-27 16:46:11 -0500

    feat: add database schema

--- database/db.sql
+++ database/db.sql
@@ -1,12 +1,43 @@
-CREATE TABLE IF NOT EXISTS users {
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (32) NOT NULL UNIQUE,
-    password VARCHAR (32) NOT NULL,
-    user_type VARCHAR (32) NOT NULL CHECK (user_type value in('teacher' or 'student')),
+CREATE TABLE IF NOT EXISTS users
+(
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR(32) NOT NULL UNIQUE,
+    password   VARCHAR(32) NOT NULL,
+    user_type  VARCHAR(7)  NOT NULL,
+    CHECK (user_type LIKE '%teacher%' or 'student'),
     created_on TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-}
+);
 
-CREATE TABLE IF NOT EXISTS exams {
-    id INTEGER PRIMARY KEY,
-    
-}
+CREATE TABLE IF NOT EXISTS exams
+(
+    id          SERIAL PRIMARY KEY,
+    access_code INTEGER     NOT NULL,
+    id_creator  INTEGER     NOT NULL,
+    title       VARCHAR(32) NOT NULL,
+    description VARCHAR(32) NOT NULL,
+    CONSTRAINT fk_exams FOREIGN KEY (id_creator) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS questions
+(
+    id                  SERIAL PRIMARY KEY,
+    description         VARCHAR(32)   NOT NULL,
+    option1             VARCHAR(32)   NOT NULL,
+    option2             VARCHAR(32)   NOT NULL,
+    option3             VARCHAR(32)   NOT NULL,
+    option4             VARCHAR(32)   NOT NULL,
+    correct_answer      VARCHAR(32)   NOT NULL,
+    question_percentage NUMERIC(1, 2) NOT NULL,
+    id_exam             INTEGER       NOT NULL,
+    CONSTRAINT fk_questions FOREIGN KEY (id_exam) REFERENCES exams (id)
+);
+
+CREATE TABLE IF NOT EXISTS grades
+(
+    id      SERIAL PRIMARY KEY,
+    id_user INTEGER       NOT NULL,
+    id_exam INTEGER       NOT NULL,
+    grade   NUMERIC(1, 2) NOT NULL,
+    CONSTRAINT fk_grades1 FOREIGN KEY (id_user) REFERENCES users (id),
+    CONSTRAINT fk_grades2 FOREIGN KEY (id_exam) REFERENCES exams (id)
+);
