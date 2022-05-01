34e6ae7816856d8abd23b746ef34375354f886ab
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-05-01 15:50:01 -0500

    adding more options

---
+++ src/pages/student/examStundent.js
@@ -0,0 +1,35 @@
+import examForm from "../../components/ExamForm";
+import Header from "../../components/Header";
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+
+export default function exam(){
+    return(
+        <body>
+        <div className="container w-75 bg-secondary mt-5">
+            <div className="row">
+                <div className="col-3">
+                    <h2 className="fw-bold text-center">Realiza el examen</h2>
+                    <form role="form">
+                        <div className="form-group">
+                            <p id="text1">This is the first question</p>
+                            <div className="swal2-checkbox">
+                                <label><input type="checkbox" value=""/>Option 1</label>
+                            </div>
+                            <div className="swal2-checkbox">
+                                <label><input type="checkbox" value=""/>Option 2</label>
+                            </div>
+                            <div className="swal2-checkbox">
+                                <label><input type="checkbox" value=""/>Option 3</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </body>
+    );
+}
+
+
+
--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -7,13 +7,27 @@ import Header from "../../components/Header";
 
 export default function Student(){
     return(
-        <div>
-            <Header tittle='Student View'></Header>
-            <main className="vh-100 d-flex justify-content-center align-items-center">
 
-                <code/>
+        <body>
+            <div className="container w-75 bg-primary mt-5 rounded shadow">
+                <div className="row-cols-1">
+                    <div className="text-end"></div>
 
-            </main>
-        </div>
+                    <h2 className="fw-bold text-center">EXAM TO STUDENTS</h2>
+
+                    <form action="#">
+                        <div className="mb-4">
+                            <label htmlFor="code" className="form-label">Code of the exam</label>
+                            <input type="code" className="form-control" name="codeExam"/>
+                        </div>
+                        <div className="d-grid ">
+                            <Link href="/student/examStudent"><button type="submit" className="btn btn-primary bg-gradient">Start</button></Link>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </body>
+
     );
 }
