4aef21c77c76523f5ddf76c385cdc4e1aabc072f
David Montaño <cuatro04sr@gmail.com>
2022-05-01 20:29:02 -0500

    fix: sintaxis query on users database

--- src/pages/api/users/[username].js
+++ src/pages/api/users/[username].js
@@ -4,8 +4,8 @@ export default async (req, res) => {
     const {method, body} = req;
     switch (method) {
         case 'GET':
-            const query = 'SELECT * FROM users u WHERE u.username=' + "'" + req.query.username + "'";
-            const response = await conn.query(query);
+            const query = 'SELECT * FROM users u WHERE u.username=$1';
+            const response = await conn.query(query, [req.query.username]);
             if (response.rows[0] === undefined) {
                 return res.status(404).json({username: req.query.username});
             } else {
