0141427e2d731a3698e913ecdd146bf5345262db
ALEXJR2002 <alexssjr2002@gmail.com>
2022-05-02 01:04:17 -0500

    [id].js replaced by [access_code].js

--- src/pages/api/exams/[access_code].js
+++ src/pages/api/exams/[access_code].js
@@ -1,14 +1,14 @@
 
 import {conn} from '../../../utils/database';
 
-export default (req, res) => {
+export default async (req, res) => {
     const {method, body} = req;
     switch (method) {
         case 'GET': 
-            const query = 'SELECT * FROM exams e WHERE e.id=$1';
-            const response = conn.query(query, [req.query.id]);
+            const query = 'SELECT * FROM exams e WHERE e.acces_code=$1';
+            const response = await conn.query(query, [req.query.access_code]);
             if (response.rows[0] === undefined) {
-                return res.status(404).json({id: req.query.id});
+                return res.status(404).json({id: req.query.access_code});
             } else {
                 return res.status(200).json(response.rows[0]);
             }
