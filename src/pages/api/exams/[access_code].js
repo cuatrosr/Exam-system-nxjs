f7904e81b1612c960c378d3d0dad263efaf7ca91
ALEXJR2002 <alexssjr2002@gmail.com>
2022-05-04 20:54:26 -0500

    minor change

--- src/pages/api/exams/[access_code].js
+++ src/pages/api/exams/[access_code].js
@@ -8,7 +8,7 @@ export default async (req, res) => {
             const query = 'SELECT * FROM exams e WHERE e.acces_code=$1';
             const response = await conn.query(query, [req.query.access_code]);
             if (response.rows[0] === undefined) {
-                return res.status(404).json({id: req.query.access_code});
+                return res.status(404).json({access_code: req.query.access_code});
             } else {
                 return res.status(200).json(response.rows[0]);
             }
