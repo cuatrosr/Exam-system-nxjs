9183af3484c11e50585ecad5a7f00999d339d416
David Montaño <cuatro04sr@gmail.com>
2022-05-07 19:05:21 -0500

    fix: removed unused code

--- src/pages/api/ping.js
+++
@@ -1,6 +0,0 @@
-import {conn} from '../../utils/database'
-
-export default async (req, res) => {
-  const response = await conn.query('SELECT NOW()');
-  return res.json({message: 'pong', time: response.rows[0].now});
-};
--- src/pages/api/teacher/newExam.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    //TODO: Implement
-    const db = require('../../utils/exams_database.json');
-    
-}
